34e689abcd267c55036a2ab827ede1d6baa8c667
MSU Student <anniebachman5280@gmail.com>
2020-11-25 21:40:51 -0700

    more server.c work

--- battlebit/src/server.c
+++ battlebit/src/server.c
@@ -232,28 +232,7 @@ int handle_client_connect(int player) {
                         cb_append(p, n);
                         cb_append(p, " ");
                         server_broadcast(p);
-                      //  printf("1%s\n", n);
-                        //printf("2%s", );
-                        //cb_write(client_socket_fd, output_buffer);
-
                     }
-
-                   // server_broadcast(string);
-
-                  // printf("2%s", string);
-                //   printf("3%s", input_buffer);
-               //     printf("3%s", input_buffer->buffer);
-                //  char *string = &input_buffer->buffer;
-                   // printf("%s", string->buffer);
-                   // printf("%s", *string);
-
-                  // server_broadcast(input_buffer);
-                 // cb_print(input_buffer);
-                 // cb_print();
-                   // printf("%s", *command);
-                  // char *str = input_buffer;
-                 //  printf("%s", str);
-
                 }
                 else if (command != NULL){
                     cb_append(output_buffer, "Command was :");
@@ -353,7 +332,6 @@ int run_server() {
             if (player > 1) {
                 break;
 
-
             }
         }
     }
@@ -365,89 +343,4 @@ int server_start() {
 
     init_server();
     pthread_create(&SERVER->server_thread, NULL, (void *) run_server, NULL);
-
-
-    /* Testing with one terminal */
-
-/*    int server_socket_fd = socket(AF_INET,
-                                  SOCK_STREAM,
-                                  IPPROTO_TCP);
-    if (server_socket_fd == -1) {
-        printf("Could not create socket\n");
-    }
-
-    int yes = 1;
-    setsockopt(server_socket_fd,
-               SOL_SOCKET,
-               SO_REUSEADDR, &yes, sizeof(yes));
-
-    struct sockaddr_in server;
-
-    // fill out the socket information
-    server.sin_family = AF_INET;
-    // bind the socket on all available interfaces
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(9876);
-
-    int request = 0;
-    if (bind(server_socket_fd,
-            // Again with the cast
-             (struct sockaddr *) &server,
-             sizeof(server)) < 0) {
-        puts("Bind failed");
-    } else {
-        puts("Bind worked!");
-        listen(server_socket_fd, 88);
-
-        //Accept an incoming connection
-        puts("Waiting for incoming connections...");
-
-        struct sockaddr_in client;
-        socklen_t size_from_connect;
-        int client_socket_fd;
-
-        while ((client_socket_fd = accept(server_socket_fd,
-                                          (struct sockaddr *) &client,
-                                          &size_from_connect)) > 0) {
-            char raw_buffer[2000];
-            char_buff *input_buffer = cb_create(2000);
-            char_buff *output_buffer = cb_create(2000);
-
-            int read_size;
-            cb_append(output_buffer, "\nbattleBit (? for help) > ");
-            cb_write(client_socket_fd, output_buffer);
-
-            while((read_size = recv(client_socket_fd, raw_buffer, 2000, 0)) > 0)
-            {
-                cb_reset(output_buffer);
-                cb_reset(input_buffer);
-                if (read_size > 0) {
-                    raw_buffer[read_size] = '\0';
-
-                    cb_append(input_buffer, raw_buffer);
-
-                    char *command = cb_tokenize(input_buffer, " \r\n");
-                    if(strcmp(command, "help") == 0)
-                    {
-                        cb_append(output_buffer, "A useful help message...");
-                        cb_append(output_buffer, command);
-
-                        cb_write(client_socket_fd, output_buffer);
-                    }
-                    else if (strcmp(command, "quit") == 0){
-                        close(client_socket_fd);
-                    }
-                    else if (command != NULL){
-                        cb_append(output_buffer, "Command was :");
-                        cb_append(output_buffer, command);
-
-                        cb_write(client_socket_fd, output_buffer);
-                    }
-                    cb_reset(output_buffer);
-                    cb_append(output_buffer, "\nbattleBit (? for hekp) > ");
-                    cb_write(client_socket_fd, output_buffer);
-                }
-            }
-        }
-    }*/
 }
